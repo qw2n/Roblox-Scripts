e0caf976af371f126d0fd72e8b0ff8e651dee1b8
qw2n <qwarlett@gmail.com>
2025-11-03 00:37:36 +1100

    Undetected Anti-Kick.lua

--- Undetected Anti-Kick.lua
+++ Undetected Anti-Kick.lua
@@ -1,106 +1,86 @@
 -- by BinaryCrypt
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/qw2n/Roblox-Scripts/refs/heads/main/Undetected%20Anti-Kick.lua"))()
 
-assert(cloneref, "Your executor not supported 'cloneref'")
-assert(clonefunction, "Your executor not supported 'clonefunction'")
-assert(hookmetamethod, "Your executor not supported 'hookmetamethod'")
-assert(newcclosure, "Your executor not supported 'newcclosure'")
-assert(getnamecallmethod, "Your executor not supported 'getnamecallmethod'")
-assert(hookfunction, "Your executor not supported 'hookfunction'")
+local function check(a, er)
+	if not a then
+		error(er)
+	end
+	
+	return a
+end
+
+local cloneref = check(cloneref, "Your executor not supported 'cloneref'")
+local newcclosure = check(newcclosure, "Your executor not supported 'newcclosure'")
+local hookfunction = check(hookfunction, "Your executor not supported 'hookfunction'")
+local clonefunction = check(clonefunction, "Your executor not supported 'clonefunction'")
+local hookmetamethod = check(hookmetamethod, "Your executor not supported 'hookmetamethod'")
+local getnamecallmethod = check(getnamecallmethod, "Your executor not supported 'getnamecallmethod'")
 
 local Players = cloneref(game:GetService("Players")) :: Players
 
 local plr = Players.LocalPlayer
 
-local oldKick, oldRemove, oldDestroy = clonefunction(plr.Kick), clonefunction(game.Remove), clonefunction(game.Destroy)
+local __namecall; __namecall = hookmetamethod(game, "__namecall", newcclosure(function(...)
+	local a = {...}
 
-local oldcall; oldcall = hookmetamethod(game, "__namecall", newcclosure(function(...)
-    local a = {...}
+	local namecall = getnamecallmethod() :: string
+	if (namecall == "Kick" or namecall == "kick") and a[1] == plr then
+		return warn("Trying kick with namecall with message:", a[2])
 
-    local namecall = getnamecallmethod() :: string
-    if (namecall == "Kick" or namecall == "kick") and a[1] == plr then
-        warn("Trying kick with namecall with message:", a[2])
+	elseif (namecall == "Remove" or namecall == "remove") and a[1] == plr then
+		return warn("Trying remove player from game using by namecall")
 
-        return
+	elseif (namecall == "Destroy" or namecall == "destroy") and a[1] == plr then
+		return warn("Trying destroy player from game using by namecall")
+	end
 
-    elseif (namecall == "Remove" or namecall == "remove") and a[1] == plr then
-        warn("Trying remove player from game using by namecall")
-
-        return
-
-    elseif (namecall == "Destroy" or namecall == "destroy") and a[1] == plr then
-        warn("Trying destroy player from game using by namecall")
-
-        return
-    end
-
-    return oldcall(...)
+	return __namecall(...)
 end))
 
-hookfunction(plr.Kick, newcclosure(function(self: Player, message: string)
-    if not self then
-        error("Expected ':' not '.' calling member function Kick", 2)
-    end
+local Kick; Kick = hookfunction(plr.Kick, newcclosure(function(self: Player, message: string)
+	if not self then
+		error("Expected ':' not '.' calling member function Kick", 2)
+	end
 
-    if type(self) ~= "userdata" then
-        error("Expected ':' not '.' calling member function Kick", 2)
-    end
+	if type(self) ~= "userdata" then
+		error("Expected ':' not '.' calling member function Kick", 2)
+	end
 
-    if not self:IsA("Player") then
-        error("Expected ':' not '.' calling member function Kick", 2)
-    end
+	if self == plr then
+		return warn("Trying kick without namecall with message:", message)
+	end
 
-    if self == plr then
-        warn("Trying kick without namecall with message:", message)
-
-        return
-    end
-
-    oldKick(self, message)
-
-    return
+	return Kick(self, message)
 end))
 
-hookfunction(game.Destroy, newcclosure(function(self: Instance)
-    if not self then
-        error("Expected ':' not '.' calling member function Destroy", 2)
+local Destroy; Destroy = hookfunction(game.Destroy, newcclosure(function(self: Instance)
+	if not self then
+		error("Expected ':' not '.' calling member function Destroy", 2)
+	end
 
-        return
-    end
+	if typeof(self) ~= "Instance" then
+		error("Expected ':' not '.' calling member function Destroy", 2)
+	end
 
-    if typeof(self) ~= "Instance" then
-        error("Expected ':' not '.' calling member function Destroy", 2)
+	if self == plr then
+		return warn("Trying destroy player from game without namecall")
+	end
 
-        return
-    end
-
-    if self:IsA("Player") and self == plr then
-        warn("Trying destroy player from game without namecall")
-
-        return
-    end
-
-    oldDestroy(self)
-
-    return
+	return Destroy(self)
 end))
 
-hookfunction(game.Remove, newcclosure(function(self: Instance)
-    if not self then
-        error("Expected ':' not '.' calling member function Remove", 2)
-    end
+local Remove; Remove = hookfunction(game.Remove, newcclosure(function(self: Instance)
+	if not self then
+		error("Expected ':' not '.' calling member function Remove", 2)
+	end
 
-    if typeof(self) ~= "Instance" then
-        error("Expected ':' not '.' calling member function Remove", 2)
-    end
+	if typeof(self) ~= "Instance" then
+		error("Expected ':' not '.' calling member function Remove", 2)
+	end
 
-    if self:IsA("Player") and self == plr then
-        warn("Trying remove player from game without namecall")
+	if self == plr then
+		return warn("Trying remove player from game without namecall")
+	end
 
-        return
-    end
-
-    oldRemove(self)
-
-    return
+	return Remove(self)
 end))
